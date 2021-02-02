703805258ac05e262f18e0dd4b9ca1acfbbef4b9
Gergely Harmadas <gergely_harmadas@epam.com>
2021-02-03 00:34:24 +0100

    continue project 1

--- projects/twenty-one.scm
+++ projects/twenty-one.scm
@@ -46,12 +46,33 @@
 (define (best-total cards)
     (define (iter total aces cards)
       (if (not (empty? cards))
-             (let ((value (first (first cards))))
+             (let ((value (bl (first cards))))
                (cond               
                  ((number? value) (iter (+ total value) aces (bf cards)))
-                 ((equal? value 'a) (iter (+ total 11) (+ aces 1) (bf cards)))
+                 ((or (equal? value 'a) (equal? value 'A)) (iter (+ total 11) (+ aces 1) (bf cards)))
                  (else (iter (+ total 10) aces (bf cards)))))
              (if (and (> total 22) (> aces 0))
                  (iter (- total 10) (- aces 1) cards)
                  total)))
     (iter 0 0 cards))
+
+(define (stop-at-17 customer-hand-so-far dealer-up-card)
+    (< (best-total customer-hand-so-far) 17))
+
+(define (play-n strategy n)
+    (if (= n 0)
+        0
+        (+ (twenty-one strategy) (play-n strategy (- n 1)))))
+
+(define (dealer-sensitive customer-hand-so-far dealer-up-card)
+    (let ((dealer-more-than-6 (> (best-total (se dealer-up-card)) 6))
+          (customer-less-than-17 (stop-at-17 customer-hand-so-far dealer-up-card))
+          (dealer-less-than-7 (< (best-total (se dealer-up-card)) 7))
+          (customer-less-than-12 (< (best-total customer-hand-so-far) 12)))
+      (let ((first-case (and dealer-more-than-6 customer-less-than-17))
+            (second-case (and dealer-less-than-7 customer-less-than-12)))
+        (or first-case second-case))))
+
+(define (stop-at n)
+    (lambda (customer-hand-so-far dealer-up-card)
+      (< (best-total customer-hand-so-far) n)))
